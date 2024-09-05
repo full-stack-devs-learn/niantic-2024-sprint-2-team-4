f3f9353e72924ac70cf26a8663c7e5789c8bf2ef
Tabatha <thabatabetts@gmail.com>
2024-09-05 14:56:11 -0700

    worked on QuizController

--- src/main/java/com/niantic/controllers/QuizController.java
+++ src/main/java/com/niantic/controllers/QuizController.java
@@ -1,12 +1,57 @@
 package com.niantic.controllers;
 
+import com.niantic.models.Quiz;
 import com.niantic.services.QuizDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-public class QuizController {
+@Controller
+@RequestMapping("/quiz")
+public class QuizController
+{
+    private QuizDao quizDao;
 
-    // ensure that the nav is associated with the quiz id
-    @GetMapping("/quiz")
-    public String
+    @Autowired
+    public QuizController(QuizDao quizDao)
+    {
+        this.quizDao = quizDao;
+    }
 
+    //Method to handle quiz selection and display quiz page:
+    @GetMapping("/{quizId}")
+    public String showQuiz(@PathVariable int quizId, Model model)
+    {
+        //get quiz data:
+        Quiz quiz = quizDao.getQuizById(quizId);
+
+        //add quiz data:
+        model.addAttribute("quiz", quiz);
+
+        //return quiz page:
+        return "quiz";
+    }
+
+    //Start the quiz and fetch first question:
+    @GetMapping("/{quizId}/start")
+    @ResponseBody
+    public Question startQuiz(@PathVariable int quizId)
+    {
+        //Fetch first question
+        return quizDao.getFirstQuestion(quizId);
+    }
+
+    //Fetch next question:
+    @GetMapping("/{quizId}/next/{questionId}")
+    @ResponseBody
+    public Question getNextQuestion(@PathVariable int quizId, @PathVariable int questionId)
+    {
+        return quizDao.getNextQuestion(quizId, questionId);
+    }
 }
+
+    
