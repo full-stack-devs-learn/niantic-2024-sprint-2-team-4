ad3d1d30046d51a6edac15bbabc0329081dbef13
Tabatha <thabatabetts@gmail.com>
2024-09-06 11:10:22 -0700

    fixed start button

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const quizzes = document.getElementById("start-quiz-btn");
-    const quizId = document.getAttribute("data-quiz-id");
+    const startButton = document.getElementById("start-quiz-btn");
+    const quizId = document.getElementById("data-quiz-id");
 
     // start button redirects page to the first question in the quiz
     startButton.addEventListener("click", () => {
-        window.location.href=`quiz/${quizId}/start`;
-    })
+        window.location.href = `quiz/${quizId}/start`;
+    });
 });
